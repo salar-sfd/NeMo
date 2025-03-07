42637abff29048fb921c3fabdb5f1d0aedeeb738
salar-sfd <142838494+salar-sfd@users.noreply.github.com>
2025-03-08 02:45:26 +0330

    Update install_beamsearch_decoders.sh
    Signed-off-by: salar-sfd <142838494+salar-sfd@users.noreply.github.com>

--- scripts/asr_language_modeling/ngram_lm/install_beamsearch_decoders.sh
+++ scripts/asr_language_modeling/ngram_lm/install_beamsearch_decoders.sh
@@ -44,15 +44,15 @@ sudo aptupdate && sudo apt-get upgrade -y && sudo apt-get install -y swig liblzm
 wget https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.tar.bz2 --no-check-certificate && tar --bzip2 -xf $NEMO_PATH/boost_1_80_0.tar.bz2 && cd boost_1_80_0 && ./bootstrap.sh && b2install --layout=tagged link=static,shared threading=multi,single install -j4 && cd .. || echo FAILURE
 export BOOST_ROOT=$NEMO_PATH/boost_1_80_0
 
-git clone https://github.com/NVIDIA/OpenSeq2Seq
+sudo git clone https://github.com/NVIDIA/OpenSeq2Seq
 cd OpenSeq2Seq
-git checkout ctc-decoders
+sudo git checkout ctc-decoders
 cd ..
-mv OpenSeq2Seq/decoders $NEMO_PATH/
+sudo mv OpenSeq2Seq/decoders $NEMO_PATH/
 rm -rf OpenSeq2Seq
 sudo chown $(whoami):$(whoami) $NEMO_PATH/decoders
 cd $NEMO_PATH/decoders
-cp $NEMO_PATH/scripts/installers/setup_os2s_decoders.py ./setup.py
+sudo cp $NEMO_PATH/scripts/installers/setup_os2s_decoders.py ./setup.py
 ./setup.sh
 
 # install KenLM
@@ -64,7 +64,7 @@ export KENLM_ROOT=$NEMO_PATH/decoders/kenlm
 cd ..
 
 # install Flashlight
-git clone https://github.com/flashlight/text && cd text
+sudo git clone https://github.com/flashlight/text && cd text
 python setup.py bdist_wheel
 pip install dist/*.whl
 cd ..
